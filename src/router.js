971ff38ce2c392ecee02036e98a45826b83a3b1b
kazacoder <avtk.tlt@gmail.com>
2024-12-16 18:01:12 +0300

    core has been created, npm init  and setting is in progress. time  34:39 lesson 2

---
+++ src/router.js
@@ -0,0 +1,71 @@
+import {Dashboard} from "./components/dashboard";
+import {Login} from "./components/login";
+import {SignUp} from "./components/sign-up";
+
+export class Router {
+    constructor() {
+        this.titlePageElement = document.getElementById('title');
+        this.contetnPageElement = document.getElementById('content');
+        this.initEvents();
+        this.routes = [
+            {
+                route: '/',
+                title: 'Дашборд',
+                filePathTemplate: '/templates/dashboard.html',
+                load: () => {
+                    new Dashboard()
+                }
+            },
+            {
+                route: '/404',
+                title: 'Страница не найдена',
+                filePathTemplate: '/templates/404.html',
+            },
+            {
+                route: '/login',
+                title: 'Авторизация',
+                filePathTemplate: '/templates/login.html',
+                load: () => {
+                    new Login()
+                }
+            },
+            {
+                route: '/sing-up',
+                title: 'Регистрация',
+                filePathTemplate: '/templates/sign-up.html',
+                load: () => {
+                    new SignUp()
+                }
+            },
+        ]
+    }
+
+    initEvents() {
+        window.addEventListener('DOMContentLoaded', this.activateRoute.bind(this));
+        window.addEventListener('popstate', this.activateRoute.bind(this));
+    }
+
+    async activateRoute() {
+        const urlRoute = window.location.pathname;
+        const newRoute = this.routes.find(item => item.route === urlRoute);
+
+        if (newRoute) {
+            if (newRoute.title) {
+                this.titlePageElement.innerText = newRoute.title + ' | Freelance Studio';
+            }
+
+            if (newRoute.filePathTemplate) {
+                this.contetnPageElement.innerHTML = await fetch(newRoute.filePathTemplate)
+                    .then(response => response.text());
+            }
+
+            if (newRoute.load && typeof newRoute.load === 'function') {
+                newRoute.load();
+            }
+        } else {
+            console.log('no route found');
+            window.location = '/404';
+
+        }
+    }
+}
